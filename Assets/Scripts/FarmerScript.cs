861db87f0f8c9e6c08c625c9066cc9120b732575
burhanyildirim7 <burhanyldrm07@gmail.com>
2022-05-29 02:31:08 +0300

    Yürüyen çiftçi yapıldı.

--- Assets/Scripts/FarmerScript.cs
+++ Assets/Scripts/FarmerScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FarmerScript : MonoBehaviour
 {
@@ -12,18 +13,32 @@ public class FarmerScript : MonoBehaviour
 
     [SerializeField] private Animator _farmerAnimator;
 
+    [SerializeField] private NavMeshAgent _agent;
+
+    private Transform _point;
+
     // Start is called before the first frame update
+    private void Awake()
+    {
+        _point = _ambarNoktasi.transform;
+    }
     void Start()
     {
         _bugdayDemeti.SetActive(true);
         _farmerAnimator.SetBool("EliBos", false);
         _farmerAnimator.SetBool("EliDolu", true);
+
+        //_agent.SetDestination(_ambarNoktasi.transform.position);
+
+        _point = _ambarNoktasi.transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-
+        if (GameController.instance.isContinue == true)
+        {
+            SetDestination(_point);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -33,16 +48,31 @@ public class FarmerScript : MonoBehaviour
             _bugdayDemeti.SetActive(false);
             _farmerAnimator.SetBool("EliDolu", false);
             _farmerAnimator.SetBool("EliBos", true);
+            _point = _tarlaNoktasi.transform;
+            //SetDestination(_tarlaNoktasi.transform);
+            //_agent.SetDestination(_tarlaNoktasi.transform.position);
+            Debug.Log("Ambarda");
         }
         else if (other.gameObject == _tarlaNoktasi)
         {
             _bugdayDemeti.SetActive(true);
             _farmerAnimator.SetBool("EliBos", false);
             _farmerAnimator.SetBool("EliDolu", true);
+            _point = _ambarNoktasi.transform;
+            //SetDestination(_ambarNoktasi.transform);
+            //_agent.SetDestination(_ambarNoktasi.transform.position);
+
+            Debug.Log("Tarlada");
         }
         else
         {
 
         }
+    }
+
+
+    private void SetDestination(Transform point)
+    {
+        _agent.SetDestination(point.position);
     }
 }
