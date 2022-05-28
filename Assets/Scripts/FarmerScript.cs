5c73e4e8b47acdb4d409d34330876de1bd2da420
burhanyildirim7 <burhanyldrm07@gmail.com>
2022-05-28 05:05:45 +0300

    Farmer Eklendi.

---
+++ Assets/Scripts/FarmerScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmerScript : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _tarlaNoktasi;
+    [SerializeField] private GameObject _ambarNoktasi;
+
+    [SerializeField] private GameObject _bugdayDemeti;
+
+    [SerializeField] private Animator _farmerAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _bugdayDemeti.SetActive(true);
+        _farmerAnimator.SetBool("EliBos", false);
+        _farmerAnimator.SetBool("EliDolu", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == _ambarNoktasi)
+        {
+            _bugdayDemeti.SetActive(false);
+            _farmerAnimator.SetBool("EliDolu", false);
+            _farmerAnimator.SetBool("EliBos", true);
+        }
+        else if (other.gameObject == _tarlaNoktasi)
+        {
+            _bugdayDemeti.SetActive(true);
+            _farmerAnimator.SetBool("EliBos", false);
+            _farmerAnimator.SetBool("EliDolu", true);
+        }
+        else
+        {
+
+        }
+    }
+}
